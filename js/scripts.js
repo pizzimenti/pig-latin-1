6c47a1f1715f20c2a00ff775a29fb24fc529ddcc
anna becker and michael smith <anna.r.becker@gmail.com>
2016-01-20 11:35:27 -0800

    add ay to end of word and removes first consonant and place it at the end

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var pigLatin = function(englishWord) {
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+   for (var i = 0; i < vowels.length; i++) {
+     if (englishWord[0] === vowels[i]) {
+     var startsWithVowel = true;
+     break;
+    }
+  }
+  if (startsWithVowel) {
+  var translatedWord = englishWord;
+} else {
+  var translatedWord = englishWord.slice(1) + englishWord[0];
+  }
+ return translatedWord + 'ay';
+};
